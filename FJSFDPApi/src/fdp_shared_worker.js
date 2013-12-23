26fdeab0794d8ea6df2be58e309666763026acf0
vovchuk <vovchuk@04686e05-bd36-44bf-aaf6-05ecb4a6ab0f>
2013-12-23 06:16:37 +0000

    FDPApi

---
+++ FJSFDPApi/src/fdp_shared_worker.js
@@ -0,0 +1,52 @@
+importScripts('fjs.core.debug.js');
+importScripts('fjs.fdp.debug.js');
+importScripts('properties.js');
+
+var connections = 0;
+var ports = [];
+
+var postToPage = function(message) {
+    for(var i=0; i<ports.length; i++) {
+        if(ports[i] && ports[i].postMessage) {
+            ports[i].postMessage(message);
+        }
+    }
+};
+
+self.addEventListener("connect", function (e) {
+    var port = e["ports"][0];
+    connections++;
+    ports.push(port);
+    port.addEventListener("message", function (e) {
+        handleMessage(e.data, function(data){
+            port.postMessage({"action":"sync", "data":data});
+        });
+    }, false);
+});
+
+function handleMessage(data, callback) {
+    switch(data.action) {
+        case "init":
+            if(!dataManager) {
+
+                var authHandler = {
+                    requestAuth:function() {
+                        postToPage({"action":"requestAuth"});
+                    }
+                    , node:function(node){
+                        postToPage({"action":"setNode", "data":{"value":node}});
+                    }
+                };
+                dataManager = new fjs.fdp.DataManager(data.data.ticket, data.data.node, self, authHandler, function(){});
+            }
+            break;
+        case "registerSync":
+            dataManager.addListener(data.feedName, callback);
+            break;
+        case "action":
+            dataManager.sendAction(data.data.feedName, data.data.actionName, data.data.data);
+            break;
+        case "logout":
+            dataManager.logout();
+    }
+};
---
+++ FJSFDPApi/src/providers/SharedWorkweDataProvider.js
@@ -0,0 +1,41 @@
+namespace("fjs.api");
+
+/**
+ * @param {string} ticket
+ * @param {string} node
+ * @param {Function} callback
+ * @constructor
+ * @extends fjs.api.ClientDataProviderBase
+ */
+fjs.api.SharedWorkerDataProvider = function(ticket, node, callback) {
+    var context =this;
+    fjs.api.ClientDataProviderBase.call(this, ticket, node);
+    this.worker = new SharedWorker("js/lib/fdp_shared_worker.js");
+    this.worker.port.addEventListener("message", function(e) {
+        context.fireEvent(e.data["action"], e.data["data"]);
+    }, false);
+    this.worker.port.addEventListener("error", function(e){
+        console.error("Worker Error", e);
+    });
+    this.worker.port.start();
+    this.sendMessage = function(message) {
+        context.worker.port.postMessage(message);
+    }
+    this.sendMessage({action:'init', data:{ticket:this.ticket, node:this.node}});
+
+    setTimeout(function(){callback()},0);
+};
+fjs.api.SharedWorkerDataProvider.extend(fjs.api.ClientDataProviderBase);
+
+/**
+ * @returns {boolean}
+ */
+fjs.api.SharedWorkerDataProvider.check = function() {
+    return  !!window.SharedWorker;
+};
+/**
+ * @param {{action:string, data:*}} message
+ */
+fjs.api.SharedWorkerDataProvider.prototype.sendMessage = function(message) {
+    this.worker.postMessage(message);
+};
