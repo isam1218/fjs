cabee5e692644f8a3a6dfe3c3ca9ab9be91169cc
vovchuk <vovchuk@04686e05-bd36-44bf-aaf6-05ecb4a6ab0f>
2013-12-26 05:44:08 +0000

    Browsers detection

--- FJSCore/src/utils/browserDetection.js
+++ FJSCore/src/utils/browserDetection.js
@@ -1,27 +1,107 @@
 namespace("fjs.utils");
 
 fjs.utils.Browser = {
-    _is_ie: null
-    , isIE:function() {
-      return this._is_ie!=null ? this._is_ie : (this._is_ie = this.getInternetExplorerVersion() != -1);
-    }
-    , getInternetExplorerVersion: function ()
+    _ieV: null
+    , getIEVersion: function ()
     {
-        var rv = -1;
-        if (navigator.appName == 'Microsoft Internet Explorer')
-        {
-            var ua = navigator.userAgent;
-            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-            if (re.exec(ua) != null)
-                rv = parseFloat( RegExp.$1 );
+        if(this._ieV==null) {
+            var rv = -1;
+            if (navigator.appName == 'Microsoft Internet Explorer')
+            {
+                var ua = navigator.userAgent;
+                var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                if (re.exec(ua) != null)
+                    rv = parseFloat( RegExp.$1 );
+            }
+            else if (navigator.appName == 'Netscape')
+            {
+                var ua = navigator.userAgent;
+                var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+                if (re.exec(ua) != null)
+                    rv = parseFloat( RegExp.$1 );
+            }
+            return this._ieV = rv;
         }
-        else if (navigator.appName == 'Netscape')
-        {
-            var ua = navigator.userAgent;
-            var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
-            if (re.exec(ua) != null)
-                rv = parseFloat( RegExp.$1 );
-        }
-        return rv;
+        return this._ieV;
     }
+    , isIE: function() {
+        if(this.getIEVersion() != -1) {
+            return function(){return true; }
+        }
+        else {
+            return function(){return false; }
+        }
+    }()
+    , isIE7: function() {
+        if(this.getIEVersion() == 7) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isIE8: function() {
+        if(this.getIEVersion() == 8) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isIE9: function() {
+        if(this.getIEVersion() == 9) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isIE10: function() {
+        if(this.getIEVersion() == 10) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isIE11: function() {
+        if(this.getIEVersion() == 11) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isChrome: function() {
+        if(/chrom(e|ium)/i.test(navigator.userAgent)) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isFirefox: function() {
+        if(/firefox/i.test(navigator.userAgent)) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isSafari: function() {
+        if(/safari/i.test(navigator.userAgent)) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
+    , isOpera: function() {
+        if(/Opera/i.test(navigator.userAgent)) {
+            return function(){return true;}
+        }
+        else {
+            return function(){return false;}
+        }
+    }()
 };
