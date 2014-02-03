4907a4bec38da9159718fafc17978106442bfa0b
vovchuk <vovchuk@04686e05-bd36-44bf-aaf6-05ecb4a6ab0f>
2014-02-03 12:19:38 +0000

    IAuthHandler

---
+++ FJSFDP/src/models/iAuthHandler.js
@@ -0,0 +1,16 @@
+namespace("fjs.db");
+
+/**
+ * @interface
+ */
+fjs.fdp.IAuthHandler = function() {
+
+};
+fjs.fdp.IAuthHandler.prototype.requestAuth = function () {
+
+}
+/**
+ * @param {string} node
+ */
+fjs.fdp.IAuthHandler.prototype.setNode = function(node) {
+}
