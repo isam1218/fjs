c41c6a2a62ee0335baae70e0f5f6915a530343c7
vovchuk <vovchuk@hud3.org>
2014-05-30 15:31:05 +0700

    HUD-451 Configure build system for SFA @clnielsen @fmeshkin @samfonality @ddiachenko @fnf

--- SFAdapter/prebuild.sh
+++ SFAdapter/prebuild.sh
@@ -2,9 +2,7 @@
 # PREBUILD SCRIPT 
 git status
 git config --global http.sslVerify false
-
 cd $WORKSPACE/SFAdapter
-
 # delete files/folders created by previous build
 for i in count.txt inject.properties build_tag *server_build_*; do
 if [[ -f $i ]]; then rm -rf $i; fi; done
@@ -13,7 +11,7 @@ if [[ -d $i ]]; then rm -rf $i; fi; done
 
 # retrieve latest build-number and set the new one - commit back to git
 git clone  https://hud_sync:_baYoUB*1121188821@bitbucket.org/fonality/hud-buildid.git
-pushd $WORKSPACE/hud-buildid
+pushd $WORKSPACE/SFAdapter/hud-buildid
     read index < count.txt
     declare -i tmp=index+1
     printf "10#%06d" $tmp > count.txt
@@ -25,11 +23,11 @@ popd
 
 
 # set GIT_TAG BUILD_NUMBER and TRIGGER_JOB_NAME envars
-cp -rf $WORKSPACE/hud-buildid/count.txt $WORKSPACE/count.txt
+cp -rf $WORKSPACE/SFAdapter/hud-buildid/count.txt $WORKSPACE/SFAdapter/count.txt
 CURRENT=`cat count.txt`
 RSTAMP=`date +%Y%m%d_%H%M`
 GIT_TAG=server_build_`echo $RSTAMP`_`echo $CURRENT`
 echo -e "GIT_TAG=$GIT_TAG
 BUILD_TIMESTAMP=$RSTAMP
-BUILD_NUMBER=`cat $WORKSPACE/hud-buildid/count.txt`
-TRIGGER_JOB_NAME=`echo $JOB_NAME`" > $WORKSPACE/inject.properties
+BUILD_NUMBER=`cat $WORKSPACE/SFAdapter/hud-buildid/count.txt`
+TRIGGER_JOB_NAME=`echo $JOB_NAME`" > $WORKSPACE/SFAdapter/inject.properties
