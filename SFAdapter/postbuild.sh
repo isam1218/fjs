4c09cb2c69af4817adfb8946adf36c3bbee8d117
victor denischik <vdenischik@fonality.com>
2014-06-02 12:59:27 -0700

    write the track commit to branchname_track

--- SFAdapter/postbuild.sh
+++ SFAdapter/postbuild.sh
@@ -3,8 +3,7 @@
 git config --global http.sslVerify false
 git tag -a $GIT_TAG -m ' '
 git push origin $GIT_TAG
-pushd hud-buildid
-echo $HEAD_COMMIT > head_commit
-git add head_commit
-git commit -m "commit anchor for changelist `date`"
-git push origin master
+echo $HEAD_COMMIT > `git rev-parse --abbrev-ref HEAD`_track
+git add `git rev-parse --abbrev-ref HEAD`_track
+git commit -m "commit anchor for Jenkins changelist"
+git push origin `git rev-parse --abbrev-ref HEAD`
