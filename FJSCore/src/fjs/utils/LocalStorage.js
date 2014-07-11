c98b4077717b529260d6b4a77b728d1d68c36ede
vovchuk <vovchuk@hud3.org>
2014-07-11 14:04:28 +0700

    #HUD-679 - SFAdapter - multitab doesn't work on safari/mac version 7.0.2

--- FJSCore/src/fjs/utils/LocalStorage.js
+++ FJSCore/src/fjs/utils/LocalStorage.js
@@ -1,15 +1,65 @@
+/**
+ * Utils static class to simplify work with LocalStorage.
+ * @constructor
+ */
 fjs.utils.LocalStorage = function() {
 
 };
+
+/**
+ * @type {boolean || null}
+ * @private
+ */
+fjs.utils.LocalStorage._enabled = null;
+
+/**
+ * Checks if localStorage exist and enabled.
+ * @returns {boolean}
+ */
 fjs.utils.LocalStorage.check = function() {
-    if(typeof self.localStorage === "undefined") return false;
+    if(fjs.utils.LocalStorage._enabled!==null) {
+        return fjs.utils.LocalStorage._enabled;
+    }
+    try {
+        if(typeof self.localStorage === "undefined")
+            return fjs.utils.LocalStorage._enabled = false;
+
         var test = 'test', val = null;
-        try {
-            localStorage.setItem(test, test);
-            val = localStorage.getItem(test);
-            localStorage.removeItem(test);
-            return !!val;
-        } catch(e) {
-            return false;
-        }
+        localStorage.setItem(test, test);
+        val = localStorage.getItem(test);
+        localStorage.removeItem(test);
+        fjs.utils.LocalStorage._enabled = !!val;
+    } catch(e) {
+        fjs.utils.LocalStorage._enabled = false;
+    }
+    return fjs.utils.LocalStorage._enabled;
 };
+
+/**
+ * @param {string} key
+ */
+fjs.utils.LocalStorage.get = function(key) {
+    if(fjs.utils.LocalStorage.check()) {
+        self.localStorage.getItem(key);
+    }
+};
+
+/**
+ * @param {string} key
+ * @param {string} value
+ */
+fjs.utils.LocalStorage.set = function(key, value) {
+    if(fjs.utils.LocalStorage.check()) {
+        self.localStorage.setItem(key, value);
+    }
+};
+
+/**
+ * @param {string} key
+ */
+fjs.utils.LocalStorage.remove = function(key) {
+    if(fjs.utils.LocalStorage.check()) {
+        self.localStorage.removeItem(key, value);
+    }
+};
+
