af4e50252067b9e0dc5462fba9d6950720522b71
cwiley <cwiley@nmetric.com>
2014-10-15 14:27:33 -0700

    Calls & Recordings

--- FJSHUD/app/scripts/controllers/CallsRecordingsController.js
+++ FJSHUD/app/scripts/controllers/CallsRecordingsController.js
@@ -2,11 +2,16 @@ fjs.core.namespace("fjs.ui");
 
 fjs.ui.CallsRecordingsController = function($scope, dataManager) {
     fjs.ui.Controller.call(this, $scope);
-    $scope.model = dataManager.getModel("mycalls");
+    $scope.model = dataManager.getModel("calllog");
     $scope.calls = $scope.model.items;
     $scope.model.addEventListener('complete', $scope.$safeApply);
     $scope.openedCallId = null;
 
+    var update = function(data) {
+        $scope.$safeApply();
+    };
+
+    $scope.model.addEventListener('push', update);
 
 };
 fjs.core.inherits(fjs.ui.CallsRecordingsController, fjs.ui.Controller)
