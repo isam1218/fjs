9b3d0c12da5281d06ff9e53cd877a65362b9444e
vovchuk <vovchuk@hud3.org>
2014-10-20 09:30:33 +0700

    build app

--- FJSHUD/Gruntfile.js
+++ FJSHUD/Gruntfile.js
@@ -1,28 +1,33 @@
 module.exports = function(grunt) {
 
-  //var currentTime = getCurrentTime()
-  //  , buildNumber;
-  //
-  //grunt.file.write('../hud-buildid/buildtimestamp.txt', currentTime);
-  //
-  //function getCurrentTime() {
-  //  if(!currentTime) {
-  //    var date = new Date();
-  //    var values = [ date.getDate(), date.getMonth() + 1, date.getHours(), date.getMinutes()];
-  //    for (var i = 0; i < values.length; i++) {
-  //      values[i] = values[i].toString().replace(/^([0-9])$/, '0$1');
-  //    }
-  //    currentTime = date.getFullYear()+values[1]+values[0]+"_"+values[2]+values[3];
-  //  }
-  //  return currentTime;
-  //}
-  //
-  //function getBuildNumber() {
-  //  if(!buildNumber) {
-  //    buildNumber = grunt.file.read('../hud-buildid/count.txt');
-  //  }
-  //  return buildNumber;
-  //}
+  var currentTime = getCurrentTime()
+    , buildNumber;
+
+  grunt.file.write('../hud-buildid/buildtimestamp.txt', currentTime);
+
+  function getCurrentTime() {
+    if(!currentTime) {
+      var date = new Date();
+      var values = [ date.getDate(), date.getMonth() + 1, date.getHours(), date.getMinutes()];
+      for (var i = 0; i < values.length; i++) {
+        values[i] = values[i].toString().replace(/^([0-9])$/, '0$1');
+      }
+      currentTime = date.getFullYear()+values[1]+values[0]+"_"+values[2]+values[3];
+    }
+    return currentTime;
+  }
+
+  function getBuildNumber() {
+    if(!buildNumber) {
+      try {
+        buildNumber = grunt.file.read('../hud-buildid/count.txt');
+      }
+      catch(e) {
+        buildNumber = -1
+      }
+    }
+    return buildNumber;
+  }
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -135,71 +140,35 @@ module.exports = function(grunt) {
       }
     }
 
-    //, 'zip': {
-    //  'sfa-zip': {
-    //    cwd: 'src/'
-    //    , src: ['src/js/bin/fjs.sf.min.js'
-    //      , 'src/js/build_number.js'
-    //      , 'src/js/lib/*.js'
-    //      , 'src/index.html'
-    //      , 'src/debug.html'
-    //      , 'src/cookiesHack.html'
-    //      , 'src/cookiesHack2.html'
-    //      , 'src/css/*.css'
-    //      , 'src/templates/*.html'
-    //      , 'src/img/*.gif'
-    //      , 'src/img/*.png'
-    //      , 'src/fonts/*'
-    //      , 'src/properties.js'
-    //      , 'src/js/salesforce_api/sf_api.js'
-    //      , 'src/js/salesforce_api/module.js'
-    //      , 'src/js/salesforce_api/SFSimpleProvider.js'
-    //      , 'src/js/salesforce_api/SFSharedWorkerProvider.js'
-    //      , 'src/js/salesforce_api/SFApiProviderFactory.js'
-    //      , 'src/js/model/feedModel.js'
-    //      , 'src/js/model/entryModel.js'
-    //      , 'src/js/model/MeModel.js'
-    //      , 'src/js/model/myCallEntryModel.js'
-    //      , 'src/js/model/myCallsFeedModel.js'
-    //      , 'src/js/model/clientSettingsFeedModel.js'
-    //      , 'src/js/model/filters/whatFilter.js'
-    //      , 'src/js/model/filters/whoFilter.js'
-    //      , 'src/js/model/filters/sortRelatedFields.js'
-    //      , 'src/js/model/dataManager.js'
-    //      , 'src/js/model/module.js'
-    //      , 'src/js/ui/controllers/commonController.js'
-    //      , 'src/js/ui/controllers/callController.js'
-    //      , 'src/js/ui/controllers/callsListController.js'
-    //      , 'src/js/ui/controllers/mainController.js'
-    //      , 'src/js/ui/controllers/newCallController.js'
-    //      , 'src/js/ui/controllers/transferDialog.js'
-    //      , 'src/js/ui/controllers/warningsController.js'
-    //      , 'src/js/ui/controllers/dialpadController.js'
-    //      , 'src/js/ui/module.js'
-    //    ]
-    //    , dest: 'bin/SFAdapter-'+getBuildNumber()+'.zip'
-    //  }
-    //}
-    //, 'string-replace':{
-    //  kit: {
-    //    files: {
-    //      'src/js/build_number.js':'src/js/build_number.js'
-    //    },
-    //    options: {
-    //      replacements: [{
-    //        pattern: /manual/gm,
-    //        replacement: getBuildNumber()
-    //      }]
-    //    }
-    //  }
-    //}
-    //,'copy': {
-    //  main: {
-    //    files: [
-    //      {expand: true, cwd: 'bin/', src: ['SFAdapter-'+getBuildNumber()+'.zip'], dest: '/media/storage/build/SFAdapter/build_'+getCurrentTime()+'_'+getBuildNumber()}
-    //    ]
-    //  }
-    //}
+    , 'zip': {
+      'sfa-zip': {
+        cwd: ''
+        , src: ['app/**/*'
+          , 'bower_components/**/*'
+        ]
+        , dest: 'bin/HUDw-'+getBuildNumber()+'.zip'
+      }
+    }
+    , 'string-replace':{
+      kit: {
+        files: {
+          'src/js/build_number.js':'src/js/build_number.js'
+        },
+        options: {
+          replacements: [{
+            pattern: /manual/gm,
+            replacement: getBuildNumber()
+          }]
+        }
+      }
+    }
+    ,'copy': {
+      main: {
+        files: [
+          {expand: true, cwd: 'bin/', src: ['HUDw-'+getBuildNumber()+'.zip'], dest: '/media/storage/build/HUDw/build_'+getCurrentTime()+'_'+getBuildNumber()}
+        ]
+      }
+    }
   });
 
 
@@ -209,6 +178,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-zip');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('build', ['concat', 'closure-compiler'/*, 'zip'*/]);
+  grunt.registerTask('build', ['concat', 'closure-compiler', 'zip']);
   grunt.registerTask('jenkins-build', ['string-replace', 'concat', 'closure-compiler', 'zip', 'copy']);
 };
