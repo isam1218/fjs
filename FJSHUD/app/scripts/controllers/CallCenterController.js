c9da96177574d46e114c898e3c330d431092a0f6
cwiley <cwiley@nmetric.com>
2014-10-24 15:35:11 -0700

    Call Center

--- FJSHUD/app/scripts/controllers/CallCenterController.js
+++ FJSHUD/app/scripts/controllers/CallCenterController.js
@@ -2,15 +2,27 @@ fjs.core.namespace("fjs.ui");
 
 fjs.ui.CallCenterController = function ($scope, dataManager) {
   fjs.ui.Controller.call(this, $scope);
-  $scope.model = dataManager.getModel("queues");
-  $scope.queues = $scope.model.items;
-  $scope.model.addEventListener('complete', function (data) {
+  $scope.queuemodel = dataManager.getModel("queues");
+  $scope.queues = $scope.queuemodel.items;
+
+  $scope.queuemodel.addEventListener('complete', function (data) {
     $scope.$safeApply();
   });
 
-
-  $scope.model.addEventListener("push", function (data) {
+  $scope.queuemodel.addEventListener("push", function (data) {
     $scope.$safeApply();
   });
+
+  $scope.queuestatsmodel = dataManager.getModel("queue_stat_calls");
+  $scope.queuestats = $scope.queuestatsmodel.items;
+
+  $scope.queuestatsmodel.addEventListener('complete', function (data) {
+    $scope.$safeApply();
+  });
+
+  $scope.queuestatsmodel.addEventListener("push", function (data) {
+    $scope.$safeApply();
+  });
+
 };
 fjs.core.inherits(fjs.ui.CallCenterController, fjs.ui.Controller)
--- FJSHUD/app/scripts/model/custom/queueEntryModel.js
+++ FJSHUD/app/scripts/model/custom/queueEntryModel.js
@@ -6,19 +6,19 @@ fjs.core.namespace("fjs.hud");
  * @extends fjs.hud.EntryModel
  * @extends fjs.fdp.QueueEntryModel
  */
-fjs.hud.QueueEntryModel = function(obj) {
+fjs.hud.QueueEntryModel = function (obj) {
 
-    fjs.hud.EntryModel.call(this, obj, 'queues');
+  fjs.hud.EntryModel.call(this, obj, 'queues');
 };
 fjs.core.inherits(fjs.hud.QueueEntryModel, fjs.hud.EntryModel);
 
-fjs.hud.QueueEntryModel.prototype.getAvatarUrl = function(width, height) {
-  if(this.fdpImage_xef001iver) {
+fjs.hud.QueueEntryModel.prototype.getAvatarUrl = function (width, height) {
+  if (this.fdpImage_xef001iver) {
     var dm = new fjs.hud.DataManager();
-    return fjs.CONFIG.SERVER.serverURL+"/v1/contact_image?pid="+this.xpid+"&w="+width+"&h="+height+"&Authorization="+dm.api.ticket+"&node="+dm.api.node+"&v="+this.fdpImage_xef001iver;
+    return fjs.CONFIG.SERVER.serverURL + "/v1/contact_image?pid=" + this.xpid + "&w=" + width + "&h=" + height + "&Authorization=" + dm.api.ticket + "&node=" + dm.api.node + "&v=" + this.fdpImage_xef001iver;
   }
   else {
-    switch(width) {
+    switch (width) {
       case 14:
         return "img/Generic-Avatar-14.png";
       case 28:
