637278779303c787b9a5915f5cd3d50d83b5da90
cwiley <cwiley@nmetric.com>
2014-12-02 14:19:44 -0800

    Initial commit for Mac native wrapper

---
+++ HUDWeb/Mac/hudweb/hudweb/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hudweb
+//
+//  Created by Chip Wiley on 11/20/14.
+//  Copyright (c) 2014 Chip Wiley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HUDWeb/Mac/hudweb/hudweb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hudweb
+//
+//  Created by Chip Wiley on 11/20/14.
+//  Copyright (c) 2014 Chip Wiley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
