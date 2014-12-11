c6e3a910e2d86a2b867ad4c62a56fbdd8fd06dda
Johnson Cheung <jcheung0@gmail.com>
2014-12-10 17:36:00 -0800

    Updated Install script for bower and npm packages

--- Tools/install.sh
+++ Tools/install.sh
@@ -1,24 +1,16 @@
-cd ../FJSCore
-npm install
-grunt
-cd ../FJSModel
-npm install
-grunt
-cd ../FJSDB
-npm install
-grunt
-cd ../FJSTabs
-npm install
-grunt
-cd ../FJSFDP
-npm install
-grunt
-cd ../FJSPlugin
-npm install
-grunt
-cd ../FJSAPI
-npm install
-grunt
-cd ../FJSHUD
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+FJ_ROOT_DIR=`pwd -P`/..
+components=( FJSCORE FJSMODEL FJSDB FJSTabs FJSFDP FJSPlugin FJSAPI )
+for comp in "${components[@]}"
+do 
+    pushd $FJ_ROOT_DIR/${comp}
+    npm install
+    grunt
+    popd
+done
+pushd $FJ_ROOT_DIR/FJSHUD
 npm install
 bower install
+popd
+popd > /dev/null
