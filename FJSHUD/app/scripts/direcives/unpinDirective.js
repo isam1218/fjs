5745754e387299a4f0accb54e5db7ff6ead5b074
Clark Nielsen <cnielsen@fonality.com>
2014-12-29 10:35:02 -0800

    css file organization

--- FJSHUD/app/scripts/direcives/unpinDirective.js
+++ FJSHUD/app/scripts/direcives/unpinDirective.js
@@ -3,17 +3,7 @@ fjs.directive.Unpin = function() {
     return function($scope,$element,$attrs){
         var el = $element[0];
         el.ondragend = function(e) {
-			/*
-            var extensionObject = document.getElementById("extensionObject");
-            var sendMessage = function(message) {
-                var event = document.createEvent("CustomEvent");
-                event.initCustomEvent("FonExtensionEvent", true, true, message);
-                this.extensionObject.dispatchEvent(event);
-            };
-            sendMessage({"action":"runApp", "url":$attrs.unpin});
-			*/
-			
-			window.open('popup.html#' + $attrs.unpin, $attrs.unpin, "scrollbars=yes, status=no, titlebar=no, toolbar=no, location=no, menubar=no, width=400, height=600");
+			//window.open('popup.html#' + $attrs.unpin, $attrs.unpin, "scrollbars=yes, status=no, titlebar=no, toolbar=no, location=no, menubar=no, width=400, height=600");
         };
 
     }
