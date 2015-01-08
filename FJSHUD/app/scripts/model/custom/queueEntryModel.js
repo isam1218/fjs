6396e9e15e6cca946105285eb8dd13c5c9d1be60
cwiley <cwiley@nmetric.com>
2015-01-07 16:20:03 -0800

    Queue Panel group by login status.

--- FJSHUD/app/scripts/model/custom/queueEntryModel.js
+++ FJSHUD/app/scripts/model/custom/queueEntryModel.js
@@ -14,22 +14,25 @@ fjs.hud.QueueEntryModel = function (obj, dm) {
 fjs.core.inherits(fjs.hud.QueueEntryModel, fjs.hud.EntryModel);
 
 fjs.hud.QueueEntryModel.prototype.getAvatar = function (memberIds, index, width, height) {
+
   if (memberIds && memberIds[index]) {
     var xpid = memberIds[index];
 
-    if(xpid) {
+    if (xpid) {
       var contact = this.dataManager.getModel('contacts').items[xpid];
 
-      return contact.getAvatarUrl(width, height);
+      if (contact) {
+        return contact.getAvatarUrl(width, height);
+      }
     }
-  } else {
-    switch (width) {
-      case 14:
-        return "img/Generic-Avatar-14.png";
-      case 28:
-        return "img/Generic-Avatar-28.png";
-      default:
-        return "img/Generic-Avatar-Small.png";
-    }
+  }
+
+  switch (width) {
+    case 14:
+      return "img/Generic-Avatar-14.png";
+    case 28:
+      return "img/Generic-Avatar-28.png";
+    default:
+      return "img/Generic-Avatar-Small.png";
   }
 };
