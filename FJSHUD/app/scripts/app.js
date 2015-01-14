685c396e170eced28ad6cb1577f5587028c8d080
Clark Nielsen <cnielsen@fonality.com>
2015-01-14 15:51:05 -0800

    HUDW-77 turned off datamanager

--- FJSHUD/app/scripts/app.js
+++ FJSHUD/app/scripts/app.js
@@ -109,7 +109,7 @@ hudweb.service('HttpService',['$http','$rootScope','$location',fjs.hud.httpServi
 
 hudweb.controller("LaunchController", ['$rootScope', '$scope', 'DataManager', fjs.ui.LaunchController]);
 hudweb.controller("MainController", ['$rootScope', '$scope', 'DataManager', 'HttpService', fjs.ui.MainController]);
-hudweb.controller("ContactsWidget", ['$scope', '$rootScope', 'DataManager', 'HttpService', fjs.ui.ContactsWidget]);
+hudweb.controller("ContactsWidget", ['$scope', '$rootScope', 'HttpService', fjs.ui.ContactsWidget]);
 hudweb.controller("GroupsController", ['$scope', '$rootScope', 'DataManager', 'HttpService', fjs.ui.GroupsController]);
 hudweb.controller("TopBarMeStatusController", ['$scope', 'DataManager', 'HttpService', fjs.ui.TopBarMeStatusController]);
 //hudweb.controller("MeWidgetController", fjs.ui.MeWidgetController);
