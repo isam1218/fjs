86c6e57c4f6717b7ec4ae77abc4b1b1f6593a239
Clark Nielsen <cnielsen@fonality.com>
2015-01-28 15:25:33 -0800

    HUDW-13 update talking total

--- FJSHUD/app/scripts/controllers/ConferencesWidgetController.js
+++ FJSHUD/app/scripts/controllers/ConferencesWidgetController.js
@@ -15,8 +15,12 @@ fjs.ui.ConferencesWidgetController = function($scope, $location, conferenceServi
 		for (i = 0; i < $scope.conferences.length; i++) {
 			if ($scope.conferences[i].members && $scope.conferences[i].members.length > 0) {
 				$scope.totals.occupied++;
-				$scope.totals.talking += $scope.conferences[i].members.length;
-				$scope.totals.all += $scope.totals.talking;
+				$scope.totals.all += $scope.conferences[i].members.length;
+				
+				for (m = 0; m < $scope.conferences[i].members.length; m++) {
+					if (!$scope.conferences[i].members[m].muted)
+						$scope.totals.talking++;
+				}
 			}
 		}
 		
