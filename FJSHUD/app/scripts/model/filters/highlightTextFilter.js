d0dc3581bdbe5bced742a614437e0a66dc3c6756
cwiley <cwiley@nmetric.com>
2015-01-29 14:47:38 -0800

    merge

--- FJSHUD/app/scripts/model/filters/highlightTextFilter.js
+++ FJSHUD/app/scripts/model/filters/highlightTextFilter.js
@@ -3,7 +3,7 @@ fjs.hud.filter.HighlightText = function($sce) {
 		if (phrase) {
 			// escape special chars
 			phrase = phrase.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
-			
+
 			text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>');
 		}
 
