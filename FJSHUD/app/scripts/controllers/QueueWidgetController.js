3516d5d926a4583cc15a05462eae8a0c320c257e
cwiley <cwiley@nmetric.com>
2015-01-28 16:58:40 -0800

    HUDW-17 - queue agents tab

--- FJSHUD/app/scripts/controllers/QueueWidgetController.js
+++ FJSHUD/app/scripts/controllers/QueueWidgetController.js
@@ -1,20 +1,45 @@
 
-fjs.ui.QueueWidgetController = function ($scope, $rootScope, $routeParams, myHttpService) {
-  $scope.queueId = $routeParams.queueId;
-  $scope.query = "";
-  $scope.sortField = "displayName";
-  $scope.sortReverse = false;
-  $scope.recents = localStorage.recents ? JSON.parse(localStorage.recents) : {};
-
-  $scope.tabs = ['Agents', 'Stats', 'Calls', 'Call Log'];
-  $scope.selected = 'Agents';
-
-  myHttpService.getFeed('queuelogoutreasons');
-  myHttpService.getFeed('queues');
-  myHttpService.getFeed('queue_members');
-  myHttpService.getFeed('queue_members_status');
-  myHttpService.getFeed('queue_stat_calls');
-
+fjs.ui.QueueWidgetController = function($scope, $rootScope, $routeParams, myHttpService) {
+    $scope.queueId = $routeParams.queueId;
+    $scope.query = "";
+    $scope.sortField = "displayName";
+    $scope.sortReverse = false;
+    $scope.recents = localStorage.recents ? JSON.parse(localStorage.recents) : {};
+    
+    $scope.tabs = ['Agents', 'Stats', 'Calls', 'Call Log'];
+    $scope.selected = 'Agents';
+    
+    myHttpService.getFeed('queuelogoutreasons');
+    myHttpService.getFeed('queues');
+    myHttpService.getFeed('queue_members');
+    myHttpService.getFeed('queue_members_status');
+    myHttpService.getFeed('queue_stat_calls');
+    
+    
+    $scope.$on('queues_updated', function(event, data) {
+        var queues = data.queues;
+        for (i = 0; i < queues.length && $scope.queue === undefined; i++) {
+            if (queues[i].xpid == $scope.queueId) {
+                $scope.queue = queues[i];
+            }
+        
+        }
+        
+        $scope.$safeApply();
+    });
+    
+    $scope.getAvatarUrl = function(index) {
+        
+        if ($scope.queue !== undefined) {
+            if ($scope.queue.members[index] !== undefined) {
+                var xpid = $scope.queue.members[index].xpid;
+                return myHttpService.get_avatar(xpid, 14, 14);
+            } 
+            else
+                return 'img/Generic-Avatar-14.png';
+        
+        }
+    };
 };
 
 fjs.core.inherits(fjs.ui.QueueWidgetController, fjs.ui.Controller);
