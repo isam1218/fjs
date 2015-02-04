6b131f4a8e3228f24336e45c9a049892eec31c6d
Clark Nielsen <cnielsen@fonality.com>
2015-02-03 16:24:40 -0800

    HUDW-121 butter hover behuvior

--- FJSHUD/app/scripts/directives/contextMenuDirective.js
+++ FJSHUD/app/scripts/directives/contextMenuDirective.js
@@ -1,28 +1,67 @@
 hudweb.directive('contextMenu', ['$rootScope', '$parse', '$timeout', function($rootScope, $parse, $timeout) {
+	var timer;
+	var current;
+
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 			var overlay = angular.element(document.getElementById('ContextMenu'));
-		
-			element.bind('mouseenter', function(e) {
-				// send object to controller
-				contact = $parse(attrs.contextMenu)(scope);
-				$rootScope.$broadcast('contextMenu', contact);
+			var rect = element[0].getBoundingClientRect();
+			
+			element.bind('mouseenter', function(e) {				
+				$timeout.cancel(timer);
 				
-				// display pop-up
-				overlay.css('display', 'block');
-				overlay.css('left', e.clientX + 'px');
-				overlay.css('top', e.clientY + 'px');
+				if (overlay.css('display') != 'block') {
+					// delay
+					timer = $timeout(function() {
+						showOverlay(e);
 				
-				overlay.bind('mouseleave', function(e) {
-					overlay.css('display', 'none');
-				});
+						overlay.bind('mouseleave', function(e) {							
+							// keep open if user moves back onto avatar
+							for (i = 0; i < element.children().length; i++)  {
+								if (e.relatedTarget == element.children()[i])
+									return;
+							}
+							
+							if (e.relatedTarget != element)
+								hideOverlay();
+						});
+						
+						overlay.bind('mouseenter', function(e) {
+							$timeout.cancel(timer);
+						});
+					}, 500);
+				}
+				else if (current != element) {
+					// hovered over a new avatar
+					showOverlay(e);
+				}
+					
+				current = element;
 			});
 			
 			element.bind('mouseleave', function(e) {
 				if (e.relatedTarget.id != 'ContextMenu')
-					overlay.css('display', 'none');
+					hideOverlay();
 			});
+			
+			function showOverlay(e) {
+				// send object to controller
+				contact = $parse(attrs.contextMenu)(scope);
+				$rootScope.$broadcast('contextMenu', contact);
+				
+				// position pop-pop
+				overlay.css('left', e.clientX + 'px');
+				overlay.css('top', e.clientY + 'px');
+				overlay.css('display', 'block');
+			}
+			
+			function hideOverlay() {
+				timer = $timeout(function() {
+					overlay.css('display', 'none');
+					overlay.unbind();
+				}, 500);
+			}
 		}
 	};
 }]);
