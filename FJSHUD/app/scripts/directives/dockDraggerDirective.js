ab2ad6720a4e0aaf50daa7630e1e9e2c0f305218
Clark Nielsen <cnielsen@fonality.com>
2015-02-19 14:56:01 -0800

    HUDW-115 switch between drag/drop styles

--- FJSHUD/app/scripts/directives/dockDraggerDirective.js
+++ FJSHUD/app/scripts/directives/dockDraggerDirective.js
@@ -1,13 +1,14 @@
 hudweb.directive('dragger', function() {
 	return {
 		restrict: 'A',
-		link: function(scope, element, attrs) {
+		link: function(scope, element, attrs) {			
 			$(element).draggable({
 				handle: '.Header, .Scrollable',
 				containment: '#DockPanel',
 				cursor: 'move',
 				cursorAt: { bottom: 0 },
-				stack: '#DockPanel .Element'
+				stack: '#DockPanel .Element',
+				connectToSortable: "#DockPanel"
 			});
 		}
 	};
