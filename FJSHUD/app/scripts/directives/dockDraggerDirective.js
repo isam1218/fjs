4589305e8e415e825e40260783b968bf949c33ed
Clark Nielsen <cnielsen@fonality.com>
2015-02-19 09:01:24 -0800

    HUDW-115 added jquery-ui for easier drag/drop

---
+++ FJSHUD/app/scripts/directives/dockDraggerDirective.js
@@ -0,0 +1,14 @@
+hudweb.directive('dragger', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			$(element).draggable({
+				handle: '.Header, .Scrollable',
+				containment: '#DockPanel',
+				cursor: 'move',
+				cursorAt: { bottom: 0 },
+				stack: '#DockPanel .Element'
+			});
+		}
+	};
+});
