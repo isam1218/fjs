504ce56b03b316e584a27f5f1466498481c5778d
Clark Nielsen <cnielsen@fonality.com>
2015-03-25 15:50:52 -0700

    HUDW-16 moved controllers around to support my status

--- FJSHUD/app/scripts/controllers/callcenter/CallCenterMyStatusController.js
+++ FJSHUD/app/scripts/controllers/callcenter/CallCenterMyStatusController.js
@@ -1,107 +1,14 @@
-hudweb.controller('CallCenterMyStatusController', ['$scope', '$rootScope', 'HttpService', 'ContactService', 'QueueService', function($scope, $rootScope, myHttpService, contactService, queueService) {
-  $scope.query = "";
-  $scope.sortField = "displayName";
-  $scope.sortReverse = false;
-  $scope.queues = [];
-  $scope.contacts = {};
-  $scope.recents = localStorage.recents ? JSON.parse(localStorage.recents) : {};
-
-  myHttpService.getFeed('queuelogoutreasons');
-  myHttpService.getFeed('queues');
-  myHttpService.getFeed('queue_members');
-  myHttpService.getFeed('queue_members_status');
-  myHttpService.getFeed('queue_stat_calls');
-
-  $scope.tabs = ['My Queue', 'All Queues', 'My Status'];
-  $scope.selected = 'My Status';
-
-  $scope.$on('queues_updated', function(event, data) {
-    $scope.queues = data.queues;
-  });
-
-
-  $scope.sort = function(field) {
-    if($scope.sortField!=field) {
-      $scope.sortField = field;
-      $scope.sortReverse = false;
-    }
-    else {
-      $scope.sortReverse = !$scope.sortReverse;
-    }
-  };
-
-  // filter contacts down
-  $scope.customFilter = function() {
-    var tab = $scope.$parent.tab;
-
-    return function(queue) {
-      // remove self
-      if (contact.xpid != $rootScope.myPid) {
-        // filter by tab
-        switch (tab) {
-          case 'all':
-            return true;
-            break;
-          case 'external':
-            if (contact.primaryExtension == '')
-              return true;
-            break;
-          case 'recent':
-            if ($scope.recents[contact.xpid] !== undefined) {
-              // attach timestamp to sort by
-              contact.timestamp = $scope.recents[contact.xpid];
-              return true;
-            }
-            break;
-          case 'queues':
-            if ($scope.queues[queue.xpid] !== undefined)
-              return true;
-            break;
-        }
-      }
-    };
-  };
-
-  $scope.customSort = function() {
-    // recent list doesn't have a sort field
-    if ($scope.$parent.tab == 'recent')
-      return 'timestamp';
-    else
-      return $scope.sortField;
-  };
-
-  $scope.customReverse = function() {
-    // recent list is always reversed
-    if ($scope.$parent.tab == 'recent')
-      return true;
-    else
-      return $scope.sortReverse;
-  };
-
-  // record most recent queue
-  $scope.storeRecent = function(xpid) {
-    $scope.recents[xpid] = new Date().getTime();
-    localStorage.recents = JSON.stringify($scope.recents);
-  };
-
-  $scope.getAvatarUrl = function(xpid) {
-    return myHttpService.get_avatar(xpid,28,28);
-  };
-
-  $scope.$on("$destroy", function() {
-
-  });
-
-  $scope.getAvatarUrl = function(queue, index) {
-
-    if(queue.members){
-      if (queue.members[index] !== undefined) {
-        var xpid = queue.members[index].xpid;
-        return myHttpService.get_avatar(xpid,14,14);
-      }
-      else
-        return 'img/Generic-Avatar-14.png';
-
-    }
-  };
+hudweb.controller('CallCenterMyStatusController', ['$scope', '$rootScope', 'HttpService', function($scope, $rootScope, httpService) {
+	
+	httpService.getFeed('queues');
+	
+	$scope.$on('queues_updated', function() {
+		angular.forEach($scope.queues, function(obj) {
+			for (i = 0; i < obj.members.length; i++) {
+				// we only care about ourselves
+				if (obj.members[i].contactId == $rootScope.myPid)
+					obj.me = obj.members[i];
+			}
+		});
+	});
 }]);
