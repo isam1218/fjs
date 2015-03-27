223fc65fcfa0d36f03b641a29db9da8f0b06acd3
Clark Nielsen <cnielsen@fonality.com>
2015-03-27 10:26:06 -0700

    HUDW-17 queue header and agents tab

--- FJSHUD/app/scripts/controllers/queues/QueueWidgetAgentsController.js
+++ FJSHUD/app/scripts/controllers/queues/QueueWidgetAgentsController.js
@@ -1,84 +1,13 @@
-hudweb.controller('QueueWidgetAgentsController', ['$scope', '$rootScope', '$routeParams', 'QueueService', 'HttpService', function ($scope, $rootScope, $routeParams, queueService, myHttpService) {
-  $scope.queueId = $routeParams.queueId;
+hudweb.controller('QueueWidgetAgentsController', ['$scope', 'ContactService', 'HttpService', function ($scope, contactService, httpService) {
   $scope.query = "";
+  $scope.selectedSort = "displayName";
 
-  $scope.contacts = [];
-  $scope.queueMembers = [];
-
-  myHttpService.getFeed('queues');
-  myHttpService.getFeed('queue_members');
-  myHttpService.getFeed('queue_members_status');
-  myHttpService.getFeed('queue_stat_calls');
-  myHttpService.getFeed('contacts');
-  myHttpService.getFeed('contacts_synced');
-
-  $scope.tabs = ['Agents', 'Stats', 'Calls', 'Call Log'];
-  $scope.selected = 'Agents';
-
-  $scope.sort_options = [
-    {display_name: "Name", type: "name"},
-    {display_name: "Call Status", type: "callStatus"},
-    {display_name: "Chat Status", type: "chatStatus"}
-  ];
-  $scope.selectedSort = $scope.sort_options[0];
-
-  $scope.sortBy = function (type) {
-    switch (type) {
-      case "name":
-        $scope.queues.sort(function (a, b) {
-          return a.name.localeCompare(b.Name);
-        });
-        break;
-      case "callStatus":
-        $scope.queues.sort(function (a, b) {
-          return b.info.waiting - a.info.waiting;
-        });
-        break;
-      case "chatStatus":
-        $scope.queues.sort(function (a, b) {
-          return a.info.waiting - b.info.waiting;
-        });
-        break;
-    }
-  };
-
-  $scope.$on('contacts_synced', function (event, data) {
-    for (key in data) {
-      var contact = data[key];
-
-      $scope.contacts[contact.xpid] = contact;
-    }
-    $rootScope.loaded = true;
-  });
-
-  $scope.$on('contactstatus_synced', function (event, data) {
-    for (key in data) {
-      for (c in $scope.contacts) {
-        // set contact's status
-        if ($scope.contacts[c].xpid == data[key].xpid) {
-          $scope.contacts[c].hud_status = data[key].xmpp;
-          break;
-        }
-      }
-    }
-  });
-
-  $scope.$on('calls_synced', function (event, data) {
-    for (key in data) {
-      for (c in $scope.contacts) {
-        // set contact's status
-        if ($scope.contacts[c].xpid == data[key].xpid) {
-          $scope.contacts[c].calls_startedAt = data[key].startedAt;
-          break;
-        }
-      }
-    }
-  });
-
-  queueService.getQueues().then(function(data) {
-	$scope.queue = queueService.getQueue($scope.queueId); 
-	myHttpService.getFeed('queue_members');
-  });
+  httpService.getFeed('queues');
+  httpService.getFeed('queue_members');
+  httpService.getFeed('queue_members_status');
+  httpService.getFeed('queue_stat_calls');
+  httpService.getFeed('contacts');
+  httpService.getFeed('contacts_synced');
 
   $scope.$on('queues_updated', function (event, data) {
     $scope.loggedInMembers = [];
@@ -86,31 +15,15 @@ hudweb.controller('QueueWidgetAgentsController', ['$scope', '$rootScope', '$rout
 	
 	if ($scope.queue && $scope.queue.members) {
 		for (var i = 0; i < $scope.queue.members.length; i++) {
-		  var member = $scope.queue.members[i];
-
-		  member.contact = $scope.contacts[member.contactId];
-
-		  if (member.status !== undefined) {
-			if (member.status.status == 'login') {
-			  member.displayStatus = "Logged in"
-			  $scope.loggedInMembers.push(member);
-			} else {
-			  member.displayStatus = "Logged out"
-			  $scope.loggedOutMembers.push(member);
-			}
-		  }
+			var member = $scope.queue.members[i];
+			
+			if (member.status.status.indexOf('login') != -1)
+				$scope.loggedInMembers.push(contactService.getContact(member.contactId));
+			else
+				$scope.loggedOutMembers.push(contactService.getContact(member.contactId));
 		}
 	}
   });
-
-  $scope.getAvatarUrl = function (xpid) {
-    if (xpid !== undefined) {
-      return myHttpService.get_avatar(xpid, 32, 32);
-    }
-    else
-      return 'img/Generic-Avatar-32.png';
-  };
-
 
   $scope.$on("$destroy", function () {
 
--- FJSHUD/app/scripts/controllers/queues/QueueWidgetController.js
+++ FJSHUD/app/scripts/controllers/queues/QueueWidgetController.js
@@ -1,18 +1,17 @@
-hudweb.controller('QueueWidgetController', ['$scope', '$rootScope', '$routeParams', 'HttpService', function($scope, $rootScope, $routeParams, myHttpService) {
+hudweb.controller('QueueWidgetController', ['$scope', '$rootScope', '$routeParams', 'HttpService', function($scope, $rootScope, $routeParams, httpService) {
     $scope.queueId = $routeParams.queueId;
     $scope.query = "";
     $scope.sortField = "displayName";
     $scope.sortReverse = false;
-    $scope.recents = localStorage.recents ? JSON.parse(localStorage.recents) : {};
     
     $scope.tabs = ['Agents', 'Stats', 'Calls', 'Call Log'];
     $scope.selected = 'Agents';
     
-    myHttpService.getFeed('queuelogoutreasons');
-    myHttpService.getFeed('queues');
-    myHttpService.getFeed('queue_members');
-    myHttpService.getFeed('queue_members_status');
-    myHttpService.getFeed('queue_stat_calls');
+    httpService.getFeed('queuelogoutreasons');
+    httpService.getFeed('queues');
+    httpService.getFeed('queue_members');
+    httpService.getFeed('queue_members_status');
+    httpService.getFeed('queue_stat_calls');
     
     
     $scope.$on('queues_updated', function(event, data) {
@@ -24,15 +23,4 @@ hudweb.controller('QueueWidgetController', ['$scope', '$rootScope', '$routeParam
         
         }
     });
-    
-    $scope.getAvatarUrl = function(index) {
-        if ($scope.queue && $scope.queue.members) {
-            if ($scope.queue.members[index] !== undefined) {
-                var xpid = $scope.queue.members[index].xpid;
-                return myHttpService.get_avatar(xpid, 14, 14);
-            } 
-            else
-                return 'img/Generic-Avatar-14.png';
-        }
-    };
 }]);
