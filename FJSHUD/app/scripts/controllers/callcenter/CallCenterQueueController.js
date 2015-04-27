e36bb3e284be751eea128f9f8472c7fcc1ae81db
Clark Nielsen <cnielsen@fonality.com>
2015-04-27 10:50:30 -0700

    comment out localStorage

--- FJSHUD/app/scripts/controllers/callcenter/CallCenterQueueController.js
+++ FJSHUD/app/scripts/controllers/callcenter/CallCenterQueueController.js
@@ -11,8 +11,8 @@ hudweb.controller('CallCenterQueueController', ['$scope', '$rootScope', 'HttpSer
     {display_name: $scope.verbage.queue_active_calls, type: "info.active"}
   ];
   
-  // $scope.selectedQueue = $scope.queue_options[0];
-  $scope.selectedQueue = localStorage.queue_option ? JSON.parse(localStorage.queue_option) : $scope.queue_options[0];
+  $scope.selectedQueue = $scope.queue_options[0];
+  // $scope.selectedQueue = localStorage.queue_option ? JSON.parse(localStorage.queue_option) : $scope.queue_options[0];
 
   $scope.sortColumn = $scope.selectedQueue.type;
   $scope.isAscending = false;
