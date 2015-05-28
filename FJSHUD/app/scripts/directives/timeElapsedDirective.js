63cbb6e3df59f643203c41341758dad6868302d7
Clark Nielsen <cnielsen@fonality.com>
2015-05-28 08:09:55 -0700

    timer directive can accept "now" @seanrose83

--- FJSHUD/app/scripts/directives/timeElapsedDirective.js
+++ FJSHUD/app/scripts/directives/timeElapsedDirective.js
@@ -5,25 +5,33 @@ hudweb.directive('timer', ['$filter', '$interval', function($filter, $interval) 
 		scope: {
 			timer: '=timer'
 		},
-		link: function(scope, element) {
+		link: function(scope, element, attrs) {
 			var loop, start, elapsed;
 			
-			// wait for value to initialize
-			scope.$watch('timer', function(data) {
-				if (data && data > 0) {
-					start = new Date(data);
-					
-					// increment every second
-					if (!loop) {
-						loop = $interval(updateDate, 1000);					
-						updateDate();
+			// start counting immediately
+			if (attrs.timer == 'now') {
+				start = new Date();
+				loop = $interval(updateDate, 1000);					
+				updateDate();
+			}
+			else {
+				// wait for value to initialize
+				scope.$watch('timer', function(data) {
+					if (data && data > 0) {
+						start = new Date(data);
+						
+						// increment every second
+						if (!loop) {
+							loop = $interval(updateDate, 1000);					
+							updateDate();
+						}
 					}
-				}
-				else {
-					element.html('---');
-					$interval.cancel(loop);
-				}
-			});
+					else {
+						element.html('---');
+						$interval.cancel(loop);
+					}
+				});
+			}
 			
 			function updateDate() {
 				elapsed = new Date().getTime() - start.getTime();
