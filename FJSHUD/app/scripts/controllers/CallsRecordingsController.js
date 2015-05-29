54a8e7a2ffc6f7be865b73fb4bfce025be988cc9
sean1rose <sean1rose@gmail.com>
2015-05-29 11:17:41 -0700

    HUDF-394: Refactored calls and recordings controller

--- FJSHUD/app/scripts/controllers/CallsRecordingsController.js
+++ FJSHUD/app/scripts/controllers/CallsRecordingsController.js
@@ -2,47 +2,33 @@ hudweb.controller('CallsRecordingsController', ['$scope', '$rootScope', '$routeP
 	// routing
 	$scope.tabs = [{upper: $scope.verbage.call_log_tab, lower: 'calllog'}, {upper:$scope.verbage.voicemail_tab, lower: 'voicemails'}, {upper: $scope.verbage.my_recordings_tab, lower: 'recordings'}];
 	
-  var getXpidInCR = $rootScope.$watch('myPid', function(newVal, oldVal){
-      if (!$scope.globalXpid){
-          $scope.globalXpid = newVal;
-              $scope.selected = localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid]) : $scope.tabs[0].lower;
-              $scope.toggleObject = localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid]) : {item: 0};
-              getXpidInCR();
-      } else {
-          getXpidInCR();
-      }
+  $scope.setFromLocalStorage = function(val){
+    $scope.globalXpid = val;
+    $scope.selected = localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid]) : $scope.tabs[0].lower;
+    $scope.toggleObject = localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid]) : {item: 0};
+  };
+
+  var getXpidInCR = $rootScope.$watch('myPid', function(newVal){
+    if (!$scope.globalXpid){
+      $scope.setFromLocalStorage(newVal);
+      getXpidInCR();
+    } else {
+      getXpidInCR();
+    }
   });
 
   $scope.$on('pidAdded', function(event, data){
-      $scope.globalXpid = data.info;
-      $scope.selected = localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid]) : $scope.tabs[0].lower;
-      $scope.toggleObject = localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_toggleObject_of_' + $scope.$scope.globalXpid]) : {item: 0}; 
+    $scope.setFromLocalStorage(data.info);
   });
 
   $scope.selected = localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid]) : $scope.tabs[0].lower;
   $scope.toggleObject = localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] ? JSON.parse(localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid]) : {item: 0};
   
   $scope.saveCRTab = function(tab, index){
-      switch(tab){
-          case "calllog":
-              $scope.selected = $scope.tabs[0].lower;
-              localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] = JSON.stringify($scope.selected);
-              $scope.toggleObject = {item: index};
-              localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] = JSON.stringify($scope.toggleObject);
-              break;
-          case "voicemails":
-              $scope.selected = $scope.tabs[1].lower;
-              localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] = JSON.stringify($scope.selected);
-              $scope.toggleObject = {item: index};
-              localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] = JSON.stringify($scope.toggleObject);
-              break;
-          case "recordings":
-              $scope.selected = $scope.tabs[2].lower;
-              localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] = JSON.stringify($scope.selected);
-              $scope.toggleObject = {item: index};
-              localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] = JSON.stringify($scope.toggleObject);
-              break;
-      }
+    $scope.selected = $scope.tabs[index].lower;
+    $scope.toggleObject = {item: index};
+    localStorage['CallsRecordings_tabs_of_' + $scope.globalXpid] = JSON.stringify($scope.selected);
+    localStorage['CallsRecordings_toggleObject_of_' + $scope.globalXpid] = JSON.stringify($scope.toggleObject);
   };
 
   $scope.tabFilter = function(){
