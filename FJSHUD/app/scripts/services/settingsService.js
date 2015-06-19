2f4f7d63ded5b4caa9506678f4f716ffe3a62e2e
Clark Nielsen <cnielsen@fonality.com>
2015-06-19 16:34:22 -0700

    INTG-22 workspace integration + contact search fixes @seanrose83

--- FJSHUD/app/scripts/services/settingsService.js
+++ FJSHUD/app/scripts/services/settingsService.js
@@ -99,7 +99,7 @@ hudweb.service('SettingsService', ['$q', '$timeout', '$rootScope', 'HttpService'
 				permissions.showCallCenter = isEnabled(data[i].propertyValue, 10);
 				// Call Center license determines whether or not a user can record
 				permissions.showVideoCollab = isEnabled(data[i].propertyValue, 1);
-				permissions.showIntellinote = false; //isEnabled(data[i].propertyValue, 15);
+				permissions.showIntellinote = isEnabled(data[i].propertyValue, 15);
 
 				// group permissions from MyPermissions.java
 				permissions.enableAgentLogin = isEnabled(data[i].propertyValue, 7);
