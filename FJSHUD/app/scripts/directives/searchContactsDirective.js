a5e7e4c5b60fff20bba3e64f99961e79abea0e08
Clark Nielsen <cnielsen@fonality.com>
2015-06-19 16:34:22 -0700

    INTG-22 workspace integration + contact search fixes @seanrose83

--- FJSHUD/app/scripts/directives/searchContactsDirective.js
+++ FJSHUD/app/scripts/directives/searchContactsDirective.js
@@ -1,23 +1,21 @@
-hudweb.directive('contactSearch', ['$document', 'ContactService', function($document, contactService) {
+hudweb.directive('contactSearch', ['$rootScope', '$document', 'ContactService', function($rootScope, $document, contactService) {
+	var contacts = [];
+
+	contactService.getContacts().then(function(data) {
+		contacts = data;
+	});
+	
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
-			var contacts = [];
-
-			// pull updates from service
-			contactService.getContacts().then(function(data) {
-				contacts = data;
-			});
 	
 			var rect = element[0].getBoundingClientRect();			
 			element.css('position', 'relative');
 			element.css('width', '100%');
+			element.css('z-index', 100);
 
-			if($(element).closest('.LeftBar').length > 0)
+			if ($(element).closest('.LeftBar').length > 0)
 				element.css('width', '95%');
-			
-			if($(element).closest('.ConferenceMembers').length == 0)
-				element.css('z-index', 100);								
 				
 			// create overlay elements
 			var overlay = angular.element('<div class="SearchContactOverlay"></div>');
@@ -28,14 +26,8 @@ hudweb.directive('contactSearch', ['$document', 'ContactService', function($docu
 			
 			var headerTitle = angular.element('<div class="Header">Add a Team Member</div>');
 
-			if(attrs.conference =="true"){
+			if (attrs.conference == "true"){
 				headerTitle = angular.element('<div class="Header">Join to conference</div>')
-				if($(element).closest('.ConferenceMembers').length > 0)
-				{	
-					overlay.css('width','450%');					
-				}	
-				else
-					overlay.css('width','300%');
 			}
 
 			// search input
@@ -48,7 +40,7 @@ hudweb.directive('contactSearch', ['$document', 'ContactService', function($docu
 					
 					// look for match
 					for (var c = 0, len = contacts.length; c < len; c++) {
-						if (contacts[c].primaryExtension && contacts[c].displayName !== undefined && contacts[c].displayName.search(new RegExp(element.val(), 'i')) != -1 || contacts[c].primaryExtension.search(new RegExp(element.val(), 'i')) != -1) {
+						if (contacts[c].xpid != $rootScope.myPid && contacts[c].primaryExtension && contacts[c].displayName !== undefined && contacts[c].displayName.search(new RegExp(element.val(), 'i')) != -1 || contacts[c].primaryExtension.search(new RegExp(element.val(), 'i')) != -1) {
 							// add to div
 							var line = makeLine(contacts[c]);
 							inset.append(line);
@@ -57,6 +49,15 @@ hudweb.directive('contactSearch', ['$document', 'ContactService', function($docu
 					
 					overlay.append(inset);
 					element.after(overlay);
+					
+					// add some paddin'
+					if (element.prop('offsetWidth') == overlay.prop('offsetWidth')) {
+						overlay.css('left', '-10px');
+						overlay.css('width', overlay.prop('offsetWidth') + 20 + 'px');
+					}
+					
+					if (element.prop('offsetTop') == overlay.prop('offsetTop'))
+						overlay.css('top', '-10px');
 			
 					// prevent accidental closing
 					overlay.bind('click', function(e) {
@@ -88,7 +89,7 @@ hudweb.directive('contactSearch', ['$document', 'ContactService', function($docu
 				line.append('<div class="Avatar AvatarSmall"><img src="' + contact.getAvatar(14) + '" onerror="this.src=\'img/Generic-Avatar-14.png\'" /></div>');
 				
 				line.append('<div class="ListRowContent"><div class="Name">' + contact.displayName + '</div><div class="Extension">#' + contact.primaryExtension + '</div></div>');
-				//line.append('<input style="float:right" class="vmcallbtn" type="button" value="Call" ng-click="callExtension('+contact.primaryExtension+')"></input>');
+				
 				// send contact to parent scope
 				line.bind('click', function() {
 					scope.searchContact(contact);
@@ -101,6 +102,7 @@ hudweb.directive('contactSearch', ['$document', 'ContactService', function($docu
 			
 			scope.$on('$destroy', function() {
 				$document.unbind('click');
+				inset.remove();
 				overlay.unbind().remove();
 			});
 		}
