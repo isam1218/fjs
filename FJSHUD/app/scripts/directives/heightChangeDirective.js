18fcb7838561ad116eb67a6b8870f1061cafab3b
razlymiles <rmiles@fonality.com>
2015-07-09 17:50:55 -0700

    HUDF-497 initialize height in the resizeHeight directive

--- FJSHUD/app/scripts/directives/heightChangeDirective.js
+++ FJSHUD/app/scripts/directives/heightChangeDirective.js
@@ -2,10 +2,14 @@ hudweb.directive('resizeHeight', function () {
     return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
+			var totalElHeights = $('.WidgetZoomBody').height() - ($('.ZoomWidgetSection.right').height() + $(".WidgetFont").height() + 60);
+	        var ttlPctUsed = Math.round((totalElHeights / $('.WidgetZoomBody').height()) * 100);
+	        $(element).css("max-height", ttlPctUsed +"%");
+	        
 			//resizes the added contacts div 
 			scope.$on('watchWindowResize::resize', function( event, data ) {		
-		        var totalElHeights = $('.WidgetZoomBody').height() - ($('.ZoomWidgetSection.right').height() + $(".WidgetFont").height() + 60);
-		        var ttlPctUsed = Math.round((totalElHeights / $('.WidgetZoomBody').height()) * 100);
+		        totalElHeights = $('.WidgetZoomBody').height() - ($('.ZoomWidgetSection.right').height() + $(".WidgetFont").height() + 60);
+		        ttlPctUsed = Math.round((totalElHeights / $('.WidgetZoomBody').height()) * 100);
 		        $(element).css("max-height", ttlPctUsed +"%");
 		    });
 		}
