1945a728d2075cec06c2b240f78ec8004cddfcca
Clark Nielsen <cnielsen@fonality.com>
2015-08-18 16:16:04 -0700

    HOW DID THAT GET BACK IN THERE!

--- FJSHUD/app/scripts/services/httpService.js
+++ FJSHUD/app/scripts/services/httpService.js
@@ -25,9 +25,7 @@ hudweb.service('HttpService', ['$http', '$rootScope', '$location', '$q', '$timeo
 	$rootScope.isFirstSync = true;
 	
 	// check for second tab before starting web worker
-	if (document.cookie.indexOf('tab=') == -1) {
-		document.cookie = 'tab=true';
-		
+	if (document.cookie.indexOf('tab=') == -1) {		
 		worker = new Worker("scripts/workers/fdpWebWorker.js");
 		
 		worker.addEventListener("message", function(event) {
