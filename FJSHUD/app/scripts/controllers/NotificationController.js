679fe84793ad17f6a08d7b40a019b28f07aaeb2a
Johnson Cheung <jcheung@fonality.com>
2015-08-25 16:59:46 -0700

    @clnielsen HUDF-790 reduced the time to cacheNotifications

--- FJSHUD/app/scripts/controllers/NotificationController.js
+++ FJSHUD/app/scripts/controllers/NotificationController.js
@@ -357,6 +357,14 @@ hudweb.controller('NotificationController',
 
   $scope.endCall = function(xpid){
     phoneService.hangUp(xpid);
+    for(var i = 0; i < $scope.calls.length;i++){
+      if($scope.calls[i].xpid == xpid){
+        $scope.calls.splice(i,1);
+        break;
+      }
+    }
+    $scope.displayAlert = true;
+    $timeout(cacheNotification,1000);
   };
 
   $scope.holdCall = function(xpid,isHeld){
@@ -592,7 +600,7 @@ hudweb.controller('NotificationController',
 		
 		if($scope.inCall)
         	$('.LeftBarNotificationSection.notificationsSection').addClass('withCalls');
-    	else
+    else
 			$('.LeftBarNotificationSection.notificationsSection').removeClass('withCalls');
 	
 		
@@ -682,16 +690,8 @@ hudweb.controller('NotificationController',
 			}
     }else{
        $scope.displayAlert = true;
-      $timeout(function(){
-        var element = document.getElementById("Alert");
-        if(element){
-          var content = element.innerHTML;
-          phoneService.cacheNotification(content,element.offsetWidth,element.offsetHeight);
-          
-        }
-        element = null;
-        $scope.displayAlert = false;
-        },2500);
+       phoneService.removeNotification();
+       $timeout(cacheNotification,1000);
     }
 
     
@@ -718,6 +718,15 @@ hudweb.controller('NotificationController',
 		$scope.displayAlert = false;
 	};
 
+  var cacheNotification = function(){
+     var element = document.getElementById("Alert");
+      if(element){
+              var content = element.innerHTML;
+              phoneService.cacheNotification(content,element.offsetWidth,element.offsetHeight);
+      }
+      element = null;
+      $scope.displayAlert = false;
+  };
 	$scope.$on('phone_event',function(event,data){
 		var phoneEvent = data.event;
 		$scope.inCall = true;
@@ -746,7 +755,7 @@ hudweb.controller('NotificationController',
 				if($scope.todaysNotifications.length > 0 || $scope.calls.length > 0){
           $scope.displayAlert = true;
           $timeout(displayNotification
-              , 2500); 
+              , 2000); 
         }
 				break;
       case "deleteChatNots":
@@ -896,16 +905,7 @@ hudweb.controller('NotificationController',
 			phoneService.removeNotification();
       if($scope.todaysNotifications.length > 0 || $scope.calls.length > 0){
           $scope.displayAlert = true;
-          $timeout(function(){
-            var element = document.getElementById("Alert");
-            if(element){
-              var content = element.innerHTML;
-              phoneService.cacheNotification(content,element.offsetWidth,element.offsetHeight);
-              
-            }
-            element = null;
-            $scope.displayAlert = false;
-            },2500);
+          $timeout(cacheNotification,2500);
     	}else{
         phoneService.cacheNotification(undefined);
         
