7801eb33f812438dedd28998656f65b344b9e4da
ishabandri <ishabandri@fonality.com>
2015-09-18 12:38:08 -0700

    removed alerts

--- FJSHUD/app/scripts/controllers/ZoomWidgetController.js
+++ FJSHUD/app/scripts/controllers/ZoomWidgetController.js
@@ -695,7 +695,7 @@ $scope.times = ["1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","
 
 ];
 var d = new Date();
-alert(d.toTimeString());
+//alert(d.toTimeString());
 //alert(d.getTimezoneOffset()/60);
  for(i=0;i<=$scope.timeZone.length;i++){
    if(d.getTimezoneOffset() == $scope.timeZone[i].offset){
