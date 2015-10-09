f6853ea744ca5653d3b4de8e35cdec8d43873077
Razly <rmiles@fonality.com>
2015-10-09 14:00:36 -0700

    HUDF-977 close the alert when the call is done @jcheungfc

--- FJSHUD/app/scripts/controllers/NotificationController.js
+++ FJSHUD/app/scripts/controllers/NotificationController.js
@@ -617,19 +617,20 @@ hudweb.controller('NotificationController',
 			}
     }else{
 
-      if(nservice.isEnabled()){
-        for (var i = 0; i < $scope.calls.length; i++){
-           nservice.dismiss("INCOMING_CALL",$scope.calls[i].xpid);   
-        } 
-      }else{
-        if($scope.calls.length > 0){
-           $scope.displayAlert = true;
-            phoneService.removeNotification();
-            $timeout(cacheNotification,1000);
-        }else{
-            phoneService.cacheNotification(undefined,0,0);
-        }  
-      }
+	      if(nservice.isEnabled()){
+	        for (var i = 0; i < $scope.calls.length; i++){
+	           nservice.dismiss("INCOMING_CALL",$scope.calls[i].xpid);   
+	        } 
+	      }else{
+	        if($scope.calls.length > 0){
+	           $scope.displayAlert = true;
+	            phoneService.removeNotification();
+	            $timeout(cacheNotification,1000);
+	        }else{	        	
+	        	phoneService.removeNotification();
+	            phoneService.cacheNotification(undefined,0,0);
+	        }  
+	      }
       }
     
 
@@ -651,12 +652,13 @@ hudweb.controller('NotificationController',
 		
     var element = document.getElementById("Alert");
 		if(element){
-			var content = element.innerHTML;
-			 if($scope.calls.length > 0 || $scope.todaysNotifications.length > 0){
-          phoneService.displayNotification(content,element.offsetWidth,element.offsetHeight);
-       }else{
-          phoneService.cacheNotification(content,element.offsetWidth,element.offsetHeight);
-        }
+				var content = element.innerHTML;
+				 if($scope.calls.length > 0 || $scope.todaysNotifications.length > 0){
+			          phoneService.displayNotification(content,element.offsetWidth,element.offsetHeight);
+			     }else{
+			    	  phoneService.removeNotification();
+			          phoneService.cacheNotification(content,element.offsetWidth,element.offsetHeight);
+			     }
 		}
 		element = null;
 		$scope.displayAlert = false;
