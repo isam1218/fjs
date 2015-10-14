f1119a80bc8aa493e795026fa6b988d3041a7f37
Razly <rmiles@fonality.com>
2015-10-14 14:00:10 -0700

    reverted some changes on properties.js

--- FJSHUD/app/properties.js
+++ FJSHUD/app/properties.js
@@ -47,8 +47,8 @@ fjs.CONFIG = {
     PLUGINS:{
         MAC:'https://hudweb.fonality.com/repository/fj.phone/1.3/res/FonalityPlugin-5.17.8986.dmg',
         WINDOWS:'https://hudweb.fonality.com/repository/fj.phone/1.3/res/FonalityPlugin-5.17.8986.msi',
-        WINDOWS_NEW:'http://repo.build01.lax01.fonality.com/hud/webphone/WebPhone-1.1.011383.msi', //'<%= WINDOWS_PLUGIN %>',
-        MAC_NEW:'http://repo.build01.lax01.fonality.com/hud/webphone/WebPhone-1.1.011382.pkg'//'<%= MAC_PLUGIN %>'
+        WINDOWS_NEW:'<%= WINDOWS_PLUGIN %>',
+        MAC_NEW:'<%= MAC_PLUGIN %>'
 
     },
     PLUGIN_VERSION:{
@@ -59,7 +59,7 @@ fjs.CONFIG = {
     },
     DEBUG:true,
 };
-fjs.CONFIG.SERVER.loginURL = "https://dev4.fon9.com:5501";
+//fjs.CONFIG.SERVER.loginURL = "https://dev4.fon9.com:5501";
 
-fjs.CONFIG.SERVER.serverURL = "https://dev4.fon9.com:8081";
+//fjs.CONFIG.SERVER.serverURL = "https://dev4.fon9.com:8081";
 
