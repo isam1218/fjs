a1853477b55b4caf4b9bf08ead92e55620a490de
vdenischik <vdenischik@fonality.com>
2015-10-15 15:44:57 -0700

    webphone versions

--- FJSHUD/app/properties.js
+++ FJSHUD/app/properties.js
@@ -54,8 +54,8 @@ fjs.CONFIG = {
     PLUGIN_VERSION:{
         MAC_OLD:'5.17.008986',
         WINDOWS_OLD:'5.17.008986',
-        WINDOWS_NEW:'1.1.011383',
-        MAC_NEW:'1.1.011382'
+        WINDOWS_NEW:'<%= WINDOWS_PLUGIN_VERSION %>',
+        MAC_NEW:'<%= MAC_PLUGIN_VERSION %>'
     },
     DEBUG:true,
 };
