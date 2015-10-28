995e866b32fa9307f009134a21b4ec9235ab9a18
Razly <rmiles@fonality.com>
2015-10-28 12:12:09 -0700

    HUDF-828 show user html tags on both notifications and chats @clnielsen

--- FJSHUD/app/scripts/controllers/ChatController.js
+++ FJSHUD/app/scripts/controllers/ChatController.js
@@ -330,8 +330,7 @@ hudweb.controller('ChatController', ['$scope','HttpService', '$routeParams', 'Co
 	// apply name and avatar and push to view
 	var addMessages = function(data) {
 		for (var i = 0, len = data.length; i < len; i++) {
-			data[i].fullProfile = contactService.getContact(data[i].from.replace('contacts:', ''));
-			//var decoded = $('<div/>').html(data[i].message).text();
+			data[i].fullProfile = contactService.getContact(data[i].from.replace('contacts:', ''));			
 			
 			if (data[i].type == 'f.conversation.chat.group.remove'){
 				data[i].message = "<strong>Goodbye " + data[i].data.groupId + "!</strong><br/>" + data[i].message;
