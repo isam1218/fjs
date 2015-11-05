90c480dee6309c8b14cb06917c41b073b856196e
Razly <rmiles@fonality.com>
2015-11-05 13:26:46 -0800

    HUDF-1239 testing

--- FJSHUD/app/scripts/controllers/MeWidgetController.js
+++ FJSHUD/app/scripts/controllers/MeWidgetController.js
@@ -515,13 +515,13 @@ hudweb.controller('MeWidgetController', ['$scope', '$rootScope', '$http', 'HttpS
     
     $scope.init_settings = function(type){
     	//var settingTime = $timeout(function(){
-    		if(typeof $scope.settings[type] != 'undefined')
-    		{    				   
+    		//if(typeof $scope.settings[type] != 'undefined')
+    		//{    				   
 	    		//$scope.$safeApply(function(){
 	    			$('#'+type).prop('checked', $scope.settings[type]);	
 	    			//$timeout.cancel(settingTime);
 	    		//});
-    		}	
+    		//}	
        // },1500);
     };
 
