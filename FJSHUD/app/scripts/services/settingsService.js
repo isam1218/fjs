1b0e40d9d24cab23af05a9102756175d09efc3a0
ishabandri <ishabandri@fonality.com>
2015-11-06 09:50:25 -0800

    added settingsService for intellinote and changed properties file

--- FJSHUD/app/scripts/services/settingsService.js
+++ FJSHUD/app/scripts/services/settingsService.js
@@ -105,7 +105,7 @@ hudweb.service('SettingsService', ['$q', '$rootScope', 'HttpService', 'ContactSe
 				permissions.showCallCenter = service.isEnabled(data[i].propertyValue, 10);
 				// Call Center license determines whether or not a user can record
 				permissions.showVideoCollab = service.isEnabled(data[i].propertyValue, 1);
-				permissions.showIntellinote = false; //isEnabled(data[i].propertyValue, 15);
+				permissions.showIntellinote = service.isEnabled(data[i].propertyValue, 15);
 				permissions.canTransferFrom = service.isEnabled(data[i].propertyValue, 4);
 
 				// group permissions from MyPermissions.java
