aeed51f83275220cc5fd1a9484ebb166e3853581
ishabandri <ishabandri@fonality.com>
2015-12-29 14:00:02 -0800

    HUDF-865 minor fixes

--- FJSHUD/app/scripts/controllers/LeftBarController.js
+++ FJSHUD/app/scripts/controllers/LeftBarController.js
@@ -13,19 +13,15 @@ hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', '
 	});
 
 	$rootScope.$on("mycalls_synced",function(event,data){
-		console.log("DATA",data);
 		
 		$scope.calls_data = data;
 		for(var i = 0; i<= data.length;i++){
 
-		if($scope.calls_data[i] != undefined && $scope.calls_data[i].incoming == false){
-		$scope.tab = 'recent';
-		$location.path("settings/callid/"+data[i].xpid);
-		}
-		else if($scope.calls_data[i].type != 0 && $scope.calls_data[i].state == 2){
-
-			$scope.tab = 'recent';
-		$location.path("settings/callid/"+data[i].xpid);
+		if($scope.calls_data[i] != undefined) {
+		 if($scope.calls_data[i].incoming == false || ($scope.calls_data[i].type != 0 && $scope.calls_data[i].state == 2)){
+		  $scope.tab = 'recent';
+		  $location.path("settings/callid/"+data[i].xpid);
+		   } 
 		}
 		}
 	});
