24292444d2a3b1e2e7f2cd42a77375174b88a0d0
ishabandri <ishabandri@fonality.com>
2015-12-28 17:00:07 -0800

    HUDF-865 switching views on calls @razlymiles

--- FJSHUD/app/scripts/controllers/LeftBarController.js
+++ FJSHUD/app/scripts/controllers/LeftBarController.js
@@ -1,4 +1,4 @@
-hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', 'PhoneService', 'SettingsService', 'StorageService', function($scope, $rootScope, httpService, phoneService, settingsService ,storageService) {
+hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', 'PhoneService', 'SettingsService', 'StorageService','$location', function($scope, $rootScope, httpService, phoneService, settingsService ,storageService,$location) {
 	$scope.query = '';
     $scope.tab = 'all';
 	$scope.overlay = '';
@@ -11,6 +11,24 @@ hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', '
 		$scope.language =  $rootScope.language;
 		$scope.$safeApply();
 	});
+
+	$rootScope.$on("mycalls_synced",function(event,data){
+		console.log("DATA",data);
+		
+		$scope.calls_data = data;
+		for(var i = 0; i<= data.length;i++){
+
+		if($scope.calls_data[i] != undefined && $scope.calls_data[i].incoming == false){
+		$scope.tab = 'recent';
+		$location.path("settings/callid/"+data[i].xpid);
+		}
+		else if($scope.calls_data[i].type != 0 && $scope.calls_data[i].state == 2){
+
+			$scope.tab = 'recent';
+		$location.path("settings/callid/"+data[i].xpid);
+		}
+		}
+	});
 	
 	$scope.setTab = function(tab) {
 		$scope.tab = tab;
@@ -21,5 +39,6 @@ hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', '
         phoneService.makeCall(number);
 		storageService.saveRecentByPhone(number);
 		$scope.number = '';
+
     };
 }]);
