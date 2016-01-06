1ef89a6579f595d779e1665f4a09ad3d53e5b750
Johnson Cheung <jcheung@fonality.com>
2016-01-06 15:38:58 -0800

    @vdenischik css generation in the install.sh

--- Tools/install.sh
+++ Tools/install.sh
@@ -11,12 +11,5 @@ bower install
 popd
 
 
-pushd $FJ_ROOT_DIR/FJSHUD/app/styles
-lessc main.less > main.css
-lessc nativeAlert.less > nativeAlert.css
-lessc ie.less > ie.css
-lessc firefox.less > firefox.css
-lessc safari.less > safari.css
-popd 
 
 popd > /dev/null
