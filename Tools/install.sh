e69758fec823886eca7017a8dd443b054efe0263
vdenischik <viktor.denischik@gmail.com>
2016-01-06 15:44:18 -0800

    fail the build if there are bower or npm errors

--- Tools/install.sh
+++ Tools/install.sh
@@ -4,10 +4,12 @@ FJ_ROOT_DIR=..
 
 pushd $FJ_ROOT_DIR/FJSHUD
 npm install
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 popd
 
 pushd $FJ_ROOT_DIR/FJSHUD/app
 bower install
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 popd
 
 
