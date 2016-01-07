1d1bebfc3fe51d37e562015a81bb5dcb7c4b014f
ishabandri <ishabandri@fonality.com>
2016-01-07 09:50:10 -0800

    conflict with cherry-pick

--- FJSHUD/app/scripts/controllers/IntellinoteController.js
+++ FJSHUD/app/scripts/controllers/IntellinoteController.js
@@ -74,7 +74,7 @@ hudweb.controller('IntellinoteController', ['$scope','$timeout', '$rootScope', '
 
 				  }).
 				  error(function(data, status, headers, config) {
-   						alert("Access error. Please contact your system administrator to Fonality Support.");
+   						alert("Access error. Please contact your system administrator or Fonality Support.");
 				  });
 	};
 	
