a5718e97088ddf539eb842d94e77943d3d32aaf0
sean1rose <sean1rose@gmail.com>
2016-01-26 10:11:53 -0800

    HUDF-321: used .all to wait for both getContacts and getSettings to resolve before attempting to load saved contact tab @clnielsen

--- FJSHUD/app/scripts/controllers/LeftBarController.js
+++ FJSHUD/app/scripts/controllers/LeftBarController.js
@@ -1,4 +1,4 @@
-hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', 'PhoneService', 'SettingsService', 'StorageService', '$timeout', function($scope, $rootScope, httpService, phoneService, settingsService ,storageService, $timeout) {
+hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', 'PhoneService', 'SettingsService', 'StorageService', '$timeout', '$q', 'ContactService', function($scope, $rootScope, httpService, phoneService, settingsService ,storageService, $timeout, $q, contactService) {
 	$scope.query = '';
   $scope.tab;
 	$scope.overlay = '';
@@ -6,33 +6,32 @@ hudweb.controller('LeftBarController', ['$scope', '$rootScope', 'HttpService', '
 	$scope.locations = [];
   $scope.language = 'us';
     
-	settingsService.getSettings().then(function(data) {		
+  $q.all([contactService.getContacts(), settingsService.getSettings()]).then(function(){
     $timeout(function(){
-      $scope.language =  $rootScope.language;
-      // load last accessed contact panel tab
+      $scope.language = $rootScope.language;
       $scope.tab = localStorage['LeftBar_tabs_of_' + $rootScope.myPid] ? JSON.parse(localStorage['LeftBar_tabs_of_' + $rootScope.myPid]) : 'all';
-    }, 2000);
+    }, 1000);
   });
   
-    $scope.setTab = function(tab) {
-      $scope.tab = tab;
-      $scope.query = '';
-      // save last accessed contact panel tab
-      localStorage['LeftBar_tabs_of_' + $rootScope.myPid] = JSON.stringify(tab);
-    };
+  $scope.setTab = function(tab) {
+    $scope.tab = tab;
+    $scope.query = '';
+    // save last accessed contact panel tab
+    localStorage['LeftBar_tabs_of_' + $rootScope.myPid] = JSON.stringify(tab);
+  };
 	
 	$scope.makeCall = function(number){
         phoneService.makeCall(number);
 		storageService.saveRecentByPhone(number);
 		$scope.number = '';
-    };
+  };
 
-    $scope.$on('locations_synced', function(event,data){
-        if(data){
-            var me = {};
-            for (var i = 0, iLen = data.length; i < iLen; i++) {
-                $scope.locations[data[i].xpid] = data[i];
-            }
-        }
-    });
+  $scope.$on('locations_synced', function(event,data){
+      if(data){
+          var me = {};
+          for (var i = 0, iLen = data.length; i < iLen; i++) {
+              $scope.locations[data[i].xpid] = data[i];
+          }
+      }
+  });
 }]);
