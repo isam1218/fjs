ea7ea090e70230b299c7951480928602fa731544
sean1rose <sean1rose@gmail.com>
2016-03-03 13:40:40 -0800

    INTG-409: Cleaned up dropbox fileshare chat msg css @ishabandri

--- FJSHUD/app/scripts/controllers/ChatController.js
+++ FJSHUD/app/scripts/controllers/ChatController.js
@@ -57,7 +57,8 @@ hudweb.controller('ChatController', ['$scope', '$rootScope', 'HttpService', '$ro
 				type: chat.type,
 				audience: chat.audience,
 				to: chat.targetId,
-				message: files[0].link,
+				message: '',
+				// message: files[0].link,
 				data: '{"attachment":[{"dropbox":true, "dropboxFile":"'+fileName+'","dropboxLink":"'+fileLink+'","fileBytes":"'+fileBytes+'"}]}'
 			});
        
@@ -82,7 +83,7 @@ hudweb.controller('ChatController', ['$scope', '$rootScope', 'HttpService', '$ro
     // only be able to select files with these extensions. You may also specify
     // file types, such as "video" or "images" in the list. For more information,
     // see File types below. By default, all extensions are allowed.
-    extensions: ['.pdf', '.doc', '.docx','.zip','.txt', '.png', '.jpg', '.jpeg'],
+    extensions: ['.pdf', '.doc', '.docx','.zip','.txt', '.png', '.jpg', '.jpeg', '.gif'],
 	};
 
 	function formatBytes(bytes,decimals) {
