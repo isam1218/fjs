10658ef7f45a49eff7834f547b05106aae90c3ac
ishabandri <ishabandri@fonality.com>
2016-03-24 10:43:08 -0700

    enabled all file extensions for dropbox and all folder types for google drive

--- FJSHUD/app/scripts/controllers/ChatController.js
+++ FJSHUD/app/scripts/controllers/ChatController.js
@@ -131,13 +131,12 @@ hudweb.controller('ChatController', ['$scope', '$rootScope', 'HttpService', '$ro
 
     // Optional. A value of false (default) limits selection to a single file, while
     // true enables multiple file selection.
-    multiselect: false, // or true
+    multiselect: false // or true
 
     // Optional. This is a list of file extensions. If specified, the user will
     // only be able to select files with these extensions. You may also specify
     // file types, such as "video" or "images" in the list. For more information,
     // see File types below. By default, all extensions are allowed.
-    extensions: ['.pdf', '.doc', '.docx','.zip','.txt', '.png', '.jpg', '.jpeg', '.gif'],
 	};
 
 	$scope.chooseDropbox = function(){
