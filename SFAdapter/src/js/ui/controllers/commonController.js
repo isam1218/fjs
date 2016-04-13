b39c71782a41a49f238973338bd7e06c4d16cdac
Razly <rmiles@fonality.com>
2016-04-13 15:01:08 -0700

    test_package started

--- SFAdapter/src/js/ui/controllers/commonController.js
+++ SFAdapter/src/js/ui/controllers/commonController.js
@@ -4,6 +4,7 @@ fjs.controllers.CommonController = function() {
 
 fjs.controllers.CommonController.prototype.safeApply = function($scope, fn) {
     var phase = $scope.$root.$$phase;
+    
     if(phase == '$apply' || phase == '$digest') {
         if(fn && (typeof(fn) === 'function')) {
             fn();
