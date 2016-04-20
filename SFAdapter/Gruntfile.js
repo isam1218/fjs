375587aad01699a4d8ec51d4f0477a2ff674287a
vdenischik <viktor.denischik@gmail.com>
2016-04-20 10:59:53 -0700

    DOP-458

--- SFAdapter/Gruntfile.js
+++ SFAdapter/Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function(grunt) {
                     , 'src/js/ui/controllers/warningsController.js'
                     , 'src/js/ui/controllers/dialpadController.js'
                     , 'src/js/ui/module.js'
-                    , 'build_nubmer'
+                    , 'src/build_nubmer'
                 ]
                 , dest: 'bin/SFAdapter-'+getBuildNumber()+'.zip'
             }
