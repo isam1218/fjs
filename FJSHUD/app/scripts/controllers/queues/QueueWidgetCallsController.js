a9784c41e32eb9bf0721f266b7454745c4d4cf1e
sean1rose <sean1rose@gmail.com>
2016-05-10 11:40:51 -0700

    HUDF-1424: STILL IN PROGRESS - this commit is for the purpose of creating a build so that Mikhail can test HUDB-152; added UI buttons to test unanswered queue transfers

--- FJSHUD/app/scripts/controllers/queues/QueueWidgetCallsController.js
+++ FJSHUD/app/scripts/controllers/queues/QueueWidgetCallsController.js
@@ -2,14 +2,14 @@ hudweb.controller('QueueWidgetCallsController', ['$scope', '$rootScope', '$route
 	$scope.queueId = $routeParams.queueId;
   $scope.que = {};
   $scope.que.query = '';
-	
+
 	$scope.callsWaiting = 0;
 	$scope.callsActive = 0;
 	$scope.longestWait = 0;
 	$scope.longestActive = 0;
 	$scope.showWaiting = true;
 	$scope.showActive = true;
-	
+
 	$scope.filterActive = function(active) {
 		return function(call) {
 			if (active && call.taken)
@@ -18,7 +18,25 @@ hudweb.controller('QueueWidgetCallsController', ['$scope', '$rootScope', '$route
 				return true;
 		}
 	};
-	
+
+	var feed = 'queue_call';
+	var action = 'transferToContact';
+	var params = {};
+
+	$scope.testTransfer = function(call){
+		params.queueCallId = call.xpid;
+		params.contactId = "100007bf0_2122674";
+		httpService.sendAction(feed, action, params);
+		console.log('Button1: Transferring to SeanTest20: feed -> ', feed, ' | action -> ', action, ' | params -> ', params);
+	};
+
+	$scope.testTransfer2 = function(call){
+		params.queueCallId = call.xpid;
+		params.contactId = "100007bf0_2122714";
+		httpService.sendAction(feed, action, params);
+		console.log('Button2: Transferring to Sean Rose: feed -> ', feed, ' | action -> ', action, ' | params -> ', params);
+	}
+
 	// listen for updates
 	$scope.$watchCollection('queue.calls', function() {
 		$scope.callsWaiting = 0;
@@ -26,21 +44,21 @@ hudweb.controller('QueueWidgetCallsController', ['$scope', '$rootScope', '$route
 
 		$scope.longestWait = ntpService.calibrateTime(new Date().getTime());
 		$scope.longestActive = ntpService.calibrateTime(new Date().getTime());
-		
+
 		for (var i = 0, iLen = $scope.queue.calls.length; i < iLen; i++) {
 			// active calls
 			if ($scope.queue.calls[i].taken) {
 				$scope.callsActive++;
-				
-				// find longest				
+
+				// find longest
 				if ($scope.queue.calls[i].startedAt < $scope.longestActive)
 					$scope.longestActive = $scope.queue.calls[i].startedAt;
 			}
 			// waiting
 			else {
 				$scope.callsWaiting++;
-						
-				// find longest				
+
+				// find longest
 				if ($scope.queue.calls[i].startedAt < $scope.longestWait)
 					$scope.longestWait = $scope.queue.calls[i].startedAt;
 			}
--- FJSHUD/app/scripts/directives/droppableDirective.js
+++ FJSHUD/app/scripts/directives/droppableDirective.js
@@ -1,22 +1,22 @@
 hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsService', '$parse', '$location', '$rootScope', function(httpService, conferenceService, settingsService ,$parse, $location, $rootScope) {
 	var timeout;
-	
+
 	// used as droppable="Type,Type,Type"
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 			if (attrs.droppable == '') return;
-			
+
 			var obj, type;
 			var drops = attrs.droppable.split(',');
-			
+
 			// element can be dropped onto
 			$(element).droppable({
 				tolerance: 'pointer',
 				greedy: true,
 				over: function(event, ui) {
 					var isolatedScope = angular.element(ui.draggable).data('_scope');
-					
+
 					// find original draggable object
 					if (ui.draggable[0].attributes.dockable)
 						obj = $parse(ui.draggable[0].attributes.dockable.nodeValue)(isolatedScope);
@@ -24,7 +24,7 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 						obj = isolatedScope.gadget.data;
 					else
 						obj = null;
-					
+
 					// get type of object
 					if (obj.firstName !== undefined)
 						type = 'Contact';
@@ -36,7 +36,7 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 						type = 'Group';
 					else if (obj.record !== undefined || obj.recorded !== undefined)
 						type = 'Call';
-					
+
 					// check for allowed cases
 					if (drops.indexOf(type) != -1 && (!$(this).hasClass('InnerDock') || obj.xpid != $rootScope.myPid)) {
 						$(this).addClass('DroppableArea');
@@ -45,23 +45,24 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 				},
 				out: function(event, ui) {
 					$(this).removeClass('DroppableArea');
-					
+
 					if ($('.DroppableArea').length == 0)
 						$(ui.helper).addClass('not-allowed');
 				},
 				drop: function(event, ui) {
 					$('.DroppableArea').removeClass('DroppableArea');
-					
+
+					// console.log('0 - ', obj);
 					// re-check basic criteria
 					if (timeout || drops.indexOf(type) == -1)
 						return;
-					
+
 					// dock new item
 					if ($(this).hasClass('InnerDock') && ui.draggable[0].attributes.dockable) {
 						// can't dock yourself
-						if (obj.xpid != $rootScope.myPid) {						
+						if (obj.xpid != $rootScope.myPid) {
 							var rect = document.getElementById('InnerDock').getBoundingClientRect();
-						
+
 							var data = {
 								name: 'GadgetConfig__empty_Gadget' + type + '_' + obj.xpid,
 								value: JSON.stringify({
@@ -69,27 +70,28 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 									"factoryId": "Gadget" + type,
 									"entityId": obj.xpid,
 									"config": {
-										"x": (ui.position.left - rect.left)/rect.width*100, 
+										"x": (ui.position.left - rect.left)/rect.width*100,
 										"y": (ui.position.top - rect.top)/rect.height*100
 									},
 									"index": $rootScope.dockIndex
 								})
 							};
-							
+
 							httpService.sendAction('settings', 'update', data);
 						}
 					}
 					// start conference via active call
 					else if (scope.$parent.currentCall) {
+						// console.log('1');
 						var currentConf = scope.$parent.currentCall.details.conferenceId;
-						
+
 						// already on a conference call
 						if (currentConf) {
 							httpService.sendAction('conferences', 'joinContact', {
 								conferenceId: currentConf,
 								contactId: obj.xpid
 							});
-							
+
 							$location.path('/conference/' + currentConf + '/currentcall');
 						}
 						// new conference
@@ -98,7 +100,7 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 								var conferences = data.conferences;
 								var found = null;
 								var len = conferences.length;
-								
+
 								// find first empty room on same server
 								for (var i = 0; i < len; i++) {
 									if (conferences[i].serverNumber.indexOf($rootScope.meModel.server_id) != -1 && conferences[i].status && (!conferences[i].members || conferences[i].members.length == 0)) {
@@ -106,7 +108,7 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 										break;
 									}
 								}
-								
+
 								// try again for linked server
 								if (!found) {
 									for (var i = 0; i < len; i++) {
@@ -117,14 +119,14 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 										}
 									}
 								}
-								
+
 								if (found) {
 									// transfer existing call
 									httpService.sendAction('mycalls', 'transferToConference', {
 										mycallId: scope.$parent.currentCall.xpid,
 										conferenceId: found
 									});
-									
+
 									// add a brief timeout before adding the second user to the conference
 									timeout = setTimeout(function() {
 										httpService.sendAction('conferences', 'joinContact', {
@@ -133,8 +135,8 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 										});
 
 									}, 2000);
-									
-									
+
+
 									$location.path('/conference/' + found + '/currentcall');
 								}
 							});
@@ -142,8 +144,9 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 					}
 					// join conference normally
 					else if (scope.conference || (scope.item && scope.item.recent_type == 'conference') || (scope.gadget && scope.gadget.name.indexOf('ConferenceRoom') != -1)) {
+					// console.log('2');
 						var xpid = scope.conference ? scope.conference.xpid : scope.gadget ? scope.gadget.data.xpid : scope.item.xpid;
-						
+
 						if (type == 'Contact') {
 							httpService.sendAction('conferences', 'joinContact', {
 								conferenceId: xpid,
@@ -155,12 +158,13 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 								mycallId: obj.xpid,
 								conferenceId: xpid,
 							});
-						
+
 							$location.path('/conference/' + xpid + '/currentcall');
 						}
 					}
 					// park call
 					else if (scope.gadget && scope.gadget.name.indexOf('GadgetParkedCalls') != -1) {
+					// console.log('3');
 						httpService.sendAction('mycalls', 'transferToPark', {
 							mycallId: obj.xpid
 						});
@@ -171,7 +175,21 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 						if (settingsService.getPermission('canTransferFrom')) {
 							var fromID;
 							var toID;
-							
+							var feed = 'queue_call';
+							var action = 'transferToContact';
+							var params = {};
+
+							// console.log('obj - ', obj);
+							// so long as queue call has not been answered yet...(transfer unanswered queue call functionality)
+							if (obj.taken == false){
+								// console.log('in if statement!');
+								params.queueCallId = obj.xpid;
+								params.contactId = scope.gadget.data.xpid;
+								httpService.sendAction(feed, action, params);
+								console.log('Drag and Drop: Transferring to -> ', scope.gadget.data.displayName, ' | feed -> ', feed, ' | action -> ', action, ' | params -> ', params);
+								return;
+							}
+
 							// queue call vs my call vs other's call
 							if (obj.agentContactId)
 								fromID = obj.agentContactId;
@@ -179,7 +197,7 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 								fromID = $rootScope.myPid;
 							else
 								fromID = obj.xpid;
-							
+
 							// contact id comes from multiple places
 							if (scope.contact)
 								toID = scope.contact.xpid;
@@ -189,14 +207,14 @@ hudweb.directive('droppable', ['HttpService', 'ConferenceService', 'SettingsServ
 								toID = scope.gadget.data.xpid;
 							else if (scope.item)
 								toID = scope.item.xpid;
-							
+
 							httpService.sendAction('calls', 'transferToContact', {
 								fromContactId: fromID,
 								toContactId: toID
 							});
 						}
 					}
-					
+
 					// prevent sibling overlap
 					timeout = setTimeout(function() {
 						timeout = null;
