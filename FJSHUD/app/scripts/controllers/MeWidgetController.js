2d0ed945a35c65ba942cb589530a57008495b835
Razly <rmiles@fonality.com>
2016-07-08 09:06:54 -0700

    HUDF-1599 added directive to remove 9-, 91- 1- from begining of a number

--- FJSHUD/app/scripts/controllers/MeWidgetController.js
+++ FJSHUD/app/scripts/controllers/MeWidgetController.js
@@ -1171,8 +1171,7 @@ hudweb.controller('MeWidgetController', ['$scope', '$rootScope', '$http', 'HttpS
 
         $scope.showOverlay(true, 'CallStatusOverlay', data);
     };
-    $scope.formatIncoming = function(calllog,type){
-    	var regStr = /^9-/;
+    $scope.formatIncoming = function(calllog,type){    	
         switch(type){
             case "From":
                 if(calllog.incoming){
@@ -1182,11 +1181,11 @@ hudweb.controller('MeWidgetController', ['$scope', '$rootScope', '$http', 'HttpS
                         return calllog.displayName;
                     }
                 }else{                	
-                    return $scope.verbage.you + " @ " + calllog.location.replace(regStr, '');
+                   return $scope.verbage.you + " @ " + calllog.location; 
                 }
             case "To":
                 if(calllog.incoming){                	
-                    return $scope.verbage.you + " @ " + calllog.location.replace(regStr, '');
+                    return $scope.verbage.you + " @ " + calllog.location;
                 }else{
                     if(calllog.phone){
                         return calllog.phone;
