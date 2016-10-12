a3438605863b0f9b225dec563a26396c4eb6b383
Isam Shabandri <ishabandri@fonality.com>
2016-10-12 13:41:26 -0700

    HUDF-1958 fix

--- FJSHUD/app/scripts/directives/callStatusDirective.js
+++ FJSHUD/app/scripts/directives/callStatusDirective.js
@@ -22,7 +22,7 @@ hudweb.directive('callstatus', ['$parse','$compile', '$location', 'NtpService','
 				scope.callstatusContact = contact;
 				if(contact.call){
 					var regStr = /^(9-|91-|1-)/;	
-					var innerElement = "<avatar ng-click='showCallOverlay(callstatusContact)' context='callstatus' profile='callstatusContact.call' type=" + contact.call.type + "> </avatar> <div class='CallLineTop'> <div class='CallerName'>" + contact.call.displayName.replace(regStr, '') + "</div> <div class='CallExtension'>" + contact.call.phone + "</div> </div> <div class='CallDuration' timer=" + contact.call.startedAt + "> </div>";
+					var innerElement = "<avatar ng-click='showCallOverlay($event,callstatusContact)' context='callstatus' profile='callstatusContact.call' type=" + contact.call.type + "> </avatar> <div class='CallLineTop'> <div class='CallerName'>" + contact.call.displayName.replace(regStr, '') + "</div> <div class='CallExtension'>" + contact.call.phone + "</div> </div> <div class='CallDuration' timer=" + contact.call.startedAt + "> </div>";
 					element.append(innerElement);
 				}else{
 					var innerElement = '<div class="NoCallText">' + scope.verbage.no_active_call + '</div> <div class="NoCallIcon"> </div>';
@@ -31,32 +31,23 @@ hudweb.directive('callstatus', ['$parse','$compile', '$location', 'NtpService','
 				$compile(element.contents())(scope);
 			}
 
-			scope.showCallOverlay = function(contact){
+			scope.showCallOverlay = function(event,contact){
 				event.stopPropagation();
 				event.preventDefault();
-				// if user doesn't have permission to view calls show overlay otherwise if user is on a conference call with permission to view calls route to conference room.
+				// if this service-function returns true -> it's a trap! User is trying to click on own cso so do not show
 				if(contact.call.displayName == "Private"){
 			      scope.showOverlay(true, 'CallStatusOverlay', contact);
 			    }
 			    else if(contact.call.details.conferenceId != undefined){
 			    $location.path("/conference/" + contact.call.details.conferenceId + "/currentcall");
 			    }
-				// if this service-function returns true -> it's a trap! User is trying to click on own cso so do not show
+
 				if (callStatusService.blockOverlay(contact)){
 					return;
 				} else {
-					// otherwise -> display...
-					switch(contact.call.type){
-						case fjs.CONFIG.CALL_TYPES.CONFERENCE_CALL:
-							var conference = conferenceService.getConference(contact.call.details.conferenceId);
-							if(conference && conference.permissions == 0){
-								$location.path('/conference/'+ contact.call.details.conferenceId);
-							}
-							break;
-						default:
-							scope.showOverlay(true, 'CallStatusOverlay', contact);
-					}
-
+					// otherwise -> display...	
+					scope.showOverlay(true, 'CallStatusOverlay', contact);
+					
 				}
 			};
 
